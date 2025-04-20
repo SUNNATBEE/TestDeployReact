27685b08104eb6dbd42623b527f0b3bac103b219
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2025-04-20 21:06:43 +0500

    salom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-amber-400 text-center font-bold'>App</div>
+    <div className='bg-amber-400 text-center font-bold'>
+    
+    App
+    <p>vercel</p>
+    </div>
+    
   )
 }
 
